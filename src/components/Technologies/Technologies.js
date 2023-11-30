0f48664c5ad9f8f25bb1b2674001b283c7383593
Austin <austin.amaral3@gmail.com>
2023-11-30 13:07:42 -0800

    Technologies section done

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,60 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider/>
+    <br/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range of technologies in the web development world.
+      Coming from a Full Stack Web Development Course, I have a great understanding of both front and back end technologies.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem"/>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            React.js <br/>
+            Tailwind <br/>
+            Redux <br/>
+            Hooks <br/>
+            Context API <br/>
+            Jest <br/>
+            Cypress <br/>
+            Yum <br/>
+            Axios <br/>
+            TypeScript <br/>
+            JavaScript <br/>
+            HTML <br/>
+            CSS <br/>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem"/>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br/>
+            Node and Databases <br/>
+            Express <br/>
+            AWS <br/>
+            SQL <br/>
+            PostgreSQL <br/>
+            Docker <br/>
+            Git CLI <br/>
+            GitHub <br/>
+            VS Code <br/>
+            Vercel <br/>
+            Heroku <br/>
+            Netlify <br/>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
