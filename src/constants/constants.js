57c4e1b8635e248a76563fd0fb45d44a76fb44c3
Austin <austin.amaral3@gmail.com>
2023-12-14 10:36:33 -0800

    updated tags

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,9 +1,9 @@
 export const projects = [
   {
     title: 'PreppedUp',
-    description: "Using React, and Node.js I've created a mealprep serviced store (frontend only) - from start to finish. The App is called PreppedUp and it allows the user to create a plan based on their goals that is fully customizeable down to each ingredient.",
+    description: "Using React, Next, and Tailwind I've created a mealprep serviced store - from start to finish. The App is called PreppedUp and it allows the user to create a plan based on their goals that is fully customizeable down to each ingredient.",
     image: '/images/mp_homepage.png',
-    tags: ['React', 'Node'],
+    tags: ['React', 'Next.js, Tailwind', 'JavaScript'],
     source: 'https://github.com/ClickAimbot/preppedup',
     visit: 'https://preppedup.net',
     id: 0,
