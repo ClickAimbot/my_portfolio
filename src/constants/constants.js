ef15344919efff4e47a71ee6e5d8493b39d0e2df
Austin <austin.amaral3@gmail.com>
2023-12-19 19:48:50 -0800

    wip

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,17 +1,27 @@
 export const projects = [
   {
     title: 'PreppedUp',
-    description: "Using React, Next, and Tailwind I've created a mealprep serviced store - from start to finish. The App is called PreppedUp and it allows the user to create a plan based on their goals that is fully customizeable down to each ingredient.",
+    description: "Created a mealprep serviced store with an excellent user experience - from start to finish. The App is called PreppedUp and it allows the user to create a plan based on their goals that is fully customizeable down to each ingredient.",
     image: '/images/mp_homepage.png',
-    tags: ['React', 'Next.js, Tailwind', 'JavaScript'],
+    tags: ['React', 'Next.js', 'Tailwind', 'JavaScript', 'MongoDB'],
     source: 'https://github.com/ClickAimbot/preppedup',
     visit: 'https://preppedup.net',
     id: 0,
   },
+  // {
+  //   title: 'Bitcoin Tracker',
+  //   description: "Using React, Next, and Tailwind I've created a mealprep serviced store - from start to finish. The App is called PreppedUp and it allows the user to create a plan based on their goals that is fully customizeable down to each ingredient.",
+  //   image: '/images/mp_homepage.png',
+  //   tags: ['React', 'AirCode.io', 'TypeScript'],
+  //   source: 'https://github.com/ClickAimbot/preppedup',
+  //   visit: 'https://preppedup.net',
+  //   id: 0,
+  // },
 ];
 
 export const TimeLineData = [
-  { year: 2023, month: "April", text: 'Started my journey', },
-  { year: 2023, month: "November", text: 'Started my first personal project', },
+  { year: 2023, month: "April", text: 'Started my journey @ BloomTech University', },
+  { year: 2023, month: "October", text: 'Started my First Personal Project for (PreppedUp)', },
+  { year: 2023, month: "December", text: 'Graduated From BloomTech', },
   
 ];
