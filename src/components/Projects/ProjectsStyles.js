f9acbd51697911c5be3a24f93fa041538dc4946b
Austin <austin.amaral3@gmail.com>
2023-12-24 15:38:07 -0800

    reverted

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Img = styled.img`
-  width:120px;
-  height:80px;
+  width:100%;
+  height:100%;
   object-fit: cover;
   overflow: hidden;
   rounded: 10px;
